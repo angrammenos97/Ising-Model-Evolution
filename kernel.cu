610d091b8c7125ecd30dcfd4dc1b22798ab18668
Anastasios Grammenos <avgramme@ece.auth.gr>
2019-12-27 18:10:00 +0200

    fix comments

--- kernel.cu
+++ kernel.cu
@@ -73,7 +73,7 @@ int main(int argc, char* argv[])
 		printf("DONE in %fsec!\n", p_time);
 	}
 	else if (expi) {	// export data
-		printf("Saving data of each iteration. This will take some time. ");
+		printf("Saving data of each iteration. ");
 		int *G_out = (int*)malloc(npd * npd * (nk + 1) * sizeof(int));
 		memcpy(G_out, G, npd*npd*sizeof(int));	// copy data to export them later		
 		for (int i = 1; i < (nk+1); i++) {	// save data of each iteration to export them for animation
@@ -177,20 +177,18 @@ void printMatrix(void *m, int r, int c, int elem_type, char *name)
 #include <device_launch_parameters.h>
 
 #define WeightMatDim 5	// Weight Matrix Dimension
-#define FloatError 1e-6
-#define TileSize 32 // Size of tiles partitioning the matrix - each tile calculates TileSize x TileSize moments
-#define NumberOfRows 32 // Rows of each block of threads - each block is of size NumberOfRows x TileSize
+#define FloatError 1e-6	// Float error
+#define TileSize 32		// Size of tiles partitioning the matrix - each tile calculates TileSize x TileSize moments
+#define NumberOfRows 16 // Rows of each block of threads - each block is of size NumberOfRows x TileSize
 
-__device__ int state_d; // Device parameter to hold if iterations should proceed
-int state; // corresponding host parameter
+__device__ int state_d; // device parameter to hold if iterations should proceed
+int state;				// corresponding host parameter
 
 __global__ void same_matrix(void* A, void* B, int elemSize, int numElem)
 {
 	state_d = 1;
-
 	for (int i = 0; i < elemSize * numElem; i++)
-
-		if (*((int*)A + i) != *((int*)B + i)) {
+		if (*((char*)A + i) != *((char*)B + i)) {
 			state_d = 0;
 			break;
 		}
@@ -203,30 +201,24 @@ __global__ void calculateFrame(int* G_d, int* GNext_d, double* w_d, int n)
 	int y = blockIdx.y * TileSize + threadIdx.y;
 
 	/*------------------------------------------*/
-
 	if (x < n) { // Check whether thread x coordinate is out of bounds
-		for (int j = 0; j < TileSize && y < n; j += NumberOfRows) { // for every block chunk of tile in the y axis 
-
+		for (int j = 0; j < TileSize && y < n; j += NumberOfRows) { // for every block chunk of tile in the y axis
 			double influence = 0.0;			// weighted influence of neighbors
-
 			for (int i = -2; i <= 2; i++) {	// for every row of weight matrix
 				int r = (y + i + n) % n;	// wrap around top with bottom
 				for (int t = -2; t <= 2; t++) {	// for every weight of a row in weight matrix
 					int c = (x + t + n) % n;	// wrap around left with right
-					influence += *(G_d + r * n + c) * *(w_d + (i + 2) * WeightMatDim + (t + 2));	// +2 cause of the x and y offet
+					influence += *(G_d + r * n + c) * *(w_d + (i + 2) * WeightMatDim + (t + 2));	// +2 cause of the i and t offset
 				}// for t < WeightMatDim
 			}// for i < WeightMatDim
 
-
 			 /*Update state for current point*/
-
 			if (influence > FloatError)			// apply threshold for floating point error
 				GNext_d[y * n + x] = 1;
 			else                             	// apply threshold for floating point error
 				GNext_d[y * n + x] = -1;
 
 			y += NumberOfRows; // Update y coordinate as we move down the tile
-
 		}// for every j < TileSize && y within G matrix's bounds
 	}
 }
@@ -234,7 +226,6 @@ __global__ void calculateFrame(int* G_d, int* GNext_d, double* w_d, int n)
 void ising(int* G, double* w, int k, int n)
 {
 	/*Declare and initialize memory to use on device*/
-
 	int* G_d, *GNext_d; double* w_d; // Pointers to use for matrix store on device
 
 	cudaMalloc((void**)&G_d, n * n * sizeof(int));
@@ -246,14 +237,11 @@ void ising(int* G, double* w, int k, int n)
 	cudaMemcpy(w_d, w, WeightMatDim * WeightMatDim * sizeof(double), cudaMemcpyHostToDevice);
 
 	/*Declare grid and block sizes and compensate for matrix not divided with block size*/
-
 	dim3 dimBlock(NumberOfRows, TileSize);
 	dim3 dimGrid((n + dimBlock.x - 1) / dimBlock.x, (n + dimBlock.y - 1) / dimBlock.y);
 
 	/*--------------------------------------------------------------------------------*/
-
 	for (int i = 0; i < k; ++i) { // For every iteration
-
 		calculateFrame <<< dimGrid, dimBlock >>> (G_d, GNext_d, w_d, n);
 		same_matrix <<< 1, 1 >>> ((void*)G_d, (void*)GNext_d, sizeof(int), n * n);
 
